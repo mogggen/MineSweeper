d88e32ae28774f4001927d9bf2840a6314dd838c
Morgan Nyman <melon21@hotmail.se>
2020-07-04 03:08:17 +0200

    Created a class for each piece of land

--- Game.cs
+++ Game.cs
@@ -10,22 +10,20 @@ namespace MineSweeper
         int w = 10, h = 10, s = 35, m = 0; // Dimensions of button: w=width, h=height, s=size, m=margin
         Button b; // The properites of the box that was pressed.
         Point p; // The coordinate of the box that was pressed.
-        Button[,] box; // The boxes
 
-        bool[,] isMine;
+        Land[,] land;
+
         bool gameOver = false;
         int mines = 16; // Number of mines to flag.
         bool first = true, replay = false; // 
         int flags = 0; // Amount of flags.
-        int[,] count; // The Number of adjacent mines for each box.
+        
 
         public Game()
         {
             InitializeComponent();
             mines = (int)NudBombCounter.Value;
-            box = new Button[w, h];
-            count = new int[w, h];
-            isMine = new bool[w, h];
+            land = new Land[w, h];
             NudBombCounter.Value = mines;
             LblMineCounter.Text = $"mines flagged:\n{flags} / {mines}";
             LblGameStatus.Visible = false;
@@ -34,7 +32,8 @@ namespace MineSweeper
             {
                 for (int x = 0; x < w; x++)
                 {
-                    box[x, y] = new Button()
+                    land[x, y] = new Land();
+                    land[x, y].Box = new Button
                     {
                         Anchor = AnchorStyles.Top | AnchorStyles.Left,
                         Font = new Font("Microsoft sans serif", s - 19, FontStyle.Regular), // Fontsize: 16, Size: 35
@@ -44,8 +43,8 @@ namespace MineSweeper
                         Tag = new Point(x, y),
                     };
 
-                    Controls.Add(box[x, y]);
-                    box[x, y].MouseDown += Game_MouseDown;
+                    Controls.Add(land[x, y].Box);
+                    land[x, y].Box.MouseDown += Game_MouseDown;
                 }
             }
         }
@@ -65,11 +64,11 @@ namespace MineSweeper
             {
                 for (int x = 0; x < w; x++)
                 {
-                    box[x, y].BackColor =
-                    box[x, y].ForeColor = Color.White;
+                    land[x, y].Box.BackColor =
+                    land[x, y].Box.ForeColor = Color.White;
                     //replay puzzle
                     if (!replay)
-                        isMine[x, y] = false;
+                        land[x, y].IsMine = false;
                 }
             }
             gameOver = false;
@@ -95,9 +94,9 @@ namespace MineSweeper
             {
                 for (int x = 0; x < w; x++)
                 {
-                    box[x, y].BackColor = Color.White;
-                    box[x, y].ForeColor = Color.White;
-                    box[x, y].Text = count[x, y].ToString();
+                    land[x, y].Box.BackColor = 
+                    land[x, y].Box.ForeColor = Color.White;
+                    land[x, y].Box.Text = land[x, y].Count.ToString();
                     gameOver = false;
                 }
             }
@@ -123,7 +122,7 @@ namespace MineSweeper
                     {
                         b.ForeColor = b.BackColor;
                         flags--;
-                        b.Text = "" + count[p.X, p.Y];
+                        b.Text = land[p.X, p.Y].Count.ToString();
                     }
                     LblMineCounter.Text = $"mines flagged:\n{flags} / {mines}";
                 }
@@ -140,25 +139,26 @@ namespace MineSweeper
                         {
                             for (int x = 0; x < w; x++)
                             {
-                                count[x, y] = 0;
+                                land[x, y].Count = 0;
 
                                 for (int i = 0; i < 9; i++)
                                 {
+                                    if (land[x, y].IsMine) break;
                                     try
                                     {
-                                        if (isMine[x + i % 3 - 1, y + i / 3 - 1] && i != 4) count[x, y]++;
+                                        if (land[x + i % 3 - 1, y + i / 3 - 1].IsMine && i != 4) land[x, y].Count++;
                                     }
                                     catch (IndexOutOfRangeException) { }
                                 }
-                                box[x, y].Text = count[x, y].ToString();
-                                if (x != p.X || y != p.Y) box[x, y].ForeColor = Color.White;
-                                else b.ForeColor = Revealed(count[p.X, p.Y]);
+                                land[x, y].Box.Text = land[x, y].Count.ToString();
+                                if (x != p.X || y != p.Y) land[x, y].Box.ForeColor = Color.White;
+                                else b.ForeColor = Revealed(land[p.X, p.Y].Count);
                             }
                         }
                         first = false;
                     }
 
-                    if (isMine[p.X, p.Y])
+                    if (land[p.X, p.Y].IsMine)
                     {
                         b.BackColor = Color.Red;
                         gameOver = true;
@@ -175,15 +175,15 @@ namespace MineSweeper
                         {
                             for (int x = 0; x < w; x++)
                             {
-                                if (isMine[x, y] && box[x, y].Text != "F")
+                                if (land[x, y].IsMine && land[x, y].Box.Text != "F")
                                 {
-                                    box[x, y].ForeColor = Color.Black;
-                                    box[x, y].Text = "X";
+                                    land[x, y].Box.ForeColor = Color.Black;
+                                    land[x, y].Box.Text = "X";
                                 }
-                                else if (!isMine[x, y] && box[x, y].Text == "F")
+                                else if (!land[x, y].IsMine && land[x, y].Box.Text == "F")
                                 {
-                                    box[x, y].ForeColor = Color.Blue;
-                                    box[x, y].Text = ">";
+                                    land[x, y].Box.ForeColor = Color.Blue;
+                                    land[x, y].Box.Text = ">";
                                 }
                             }
                         }
@@ -197,20 +197,20 @@ namespace MineSweeper
                     int revealed = 0;
                     for (int y = 0; y < h; y++)
                         for (int x = 0; x < w; x++)
-                            if (box[x, y].BackColor == BackColor)
+                            if (land[x, y].Box.BackColor == BackColor)
                                 revealed++;
 
-                    if (revealed == w * h - mines && !isMine[p.X, p.Y])
+                    if (revealed == w * h - mines && !land[p.X, p.Y].IsMine)
                     {
                         for (int y = 0; y < h; y++)
                         {
                             for (int x = 0; x < w; x++)
                             {
-                                if (isMine[x, y] && box[x, y].Text != "F")
+                                if (land[x, y].IsMine && land[x, y].Box.Text != "F")
                                 {
-                                    box[x, y].ForeColor = Color.Red;
+                                    land[x, y].Box.ForeColor = Color.Red;
                                     flags++;
-                                    box[x, y].Text = "F";
+                                    land[x, y].Box.Text = "F";
 
                                 }
                             }
@@ -232,7 +232,7 @@ namespace MineSweeper
             {
                 for (int x = 0; x < w; x++)
                 {
-                    box[x, y].Location = new Point(Width / 2 - (w / 2 * s + (w / 2 - 1)) + x * (s + m), Height / 2 - (h / 2 * s + (h / 2 - 1)) + y * (s + m));
+                    land[x, y].Box.Location = new Point(Width / 2 - (w / 2 * s + (w / 2 - 1)) + x * (s + m), Height / 2 - (h / 2 * s + (h / 2 - 1)) + y * (s + m));
                 }
             }
         }
@@ -245,33 +245,34 @@ namespace MineSweeper
             {
                 ned = rand.Next(h);
                 sid = rand.Next(w);
-                if (!isMine[sid, ned] && (p.Y != ned || p.X != sid))
+                if (!land[sid, ned].IsMine && (p.Y != ned || p.X != sid))
                 {
-                    isMine[sid, ned] = true;
-                    box[sid, ned].Text = "X";
+                    land[sid, ned].IsMine = true;
+                    land[sid, ned].Box.Text = "X";
                     i++;
                 }
             }
         }
 
-        class Mine
+        class Land
         {
-            public int Counter { get; set; }
+            public Button Box { get; set; }
+            public int Count { get; set; }
             public bool IsFlagged { get; set; }
             public bool IsMine { get; set; }
         }
 
         void Sweep(int x, int y)
         {
-            if (box[x, y].Text == "F" ||
-                box[x, y].BackColor == BackColor) { return; }
+            if (land[x, y].Box.Text == "F" ||
+                land[x, y].Box.BackColor == BackColor) { return; }
 
-            if (box[x, y].BackColor == Color.White)
+            if (land[x, y].Box.BackColor == Color.White)
             {
-                box[x, y].BackColor = BackColor;
-                box[x, y].ForeColor = Revealed(int.Parse(box[x, y].Text));
+                land[x, y].Box.BackColor = BackColor;
+                land[x, y].Box.ForeColor = Revealed(int.Parse(land[x, y].Box.Text));
 
-                if (box[x, y].Text == "0")
+                if (land[x, y].Box.Text == "0")
                     for (int i = 0; i < 9; i++)
                         if (i != 4)
                             try
