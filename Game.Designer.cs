f33a5286e9c01abea8910b21a5d169e627d8a32d
Morgan Nyman <mornym-9@student.ltu.se>
2021-04-07 23:18:12 +0200

    didn't even cost you 2 month of MONKE

--- Game.Designer.cs
+++ Game.Designer.cs
@@ -28,100 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.GameStatus = new System.Windows.Forms.PictureBox();
-            this.NudBombCounter = new System.Windows.Forms.NumericUpDown();
-            this.LblMineCounter = new System.Windows.Forms.Label();
-            this.LblGameStatus = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.GameStatus)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.NudBombCounter)).BeginInit();
             this.SuspendLayout();
-            // 
-            // GameStatus
-            // 
-            this.GameStatus.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.GameStatus.BackColor = System.Drawing.Color.LightGray;
-            this.GameStatus.ErrorImage = null;
-            this.GameStatus.Image = global::MineSweeper.Properties.Resources.playing;
-            this.GameStatus.Location = new System.Drawing.Point(177, 23);
-            this.GameStatus.Name = "GameStatus";
-            this.GameStatus.Size = new System.Drawing.Size(100, 84);
-            this.GameStatus.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.GameStatus.TabIndex = 0;
-            this.GameStatus.TabStop = false;
-            this.GameStatus.Click += new System.EventHandler(this.GameStatus_Click);
-            // 
-            // NudBombCounter
-            // 
-            this.NudBombCounter.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.NudBombCounter.Location = new System.Drawing.Point(133, 87);
-            this.NudBombCounter.Maximum = new decimal(new int[] {
-            91,
-            0,
-            0,
-            0});
-            this.NudBombCounter.Name = "NudBombCounter";
-            this.NudBombCounter.Size = new System.Drawing.Size(38, 20);
-            this.NudBombCounter.TabIndex = 1;
-            this.NudBombCounter.Value = new decimal(new int[] {
-            16,
-            0,
-            0,
-            0});
-            // 
-            // LblMineCounter
-            // 
-            this.LblMineCounter.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.LblMineCounter.AutoSize = true;
-            this.LblMineCounter.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LblMineCounter.ForeColor = System.Drawing.Color.Black;
-            this.LblMineCounter.Location = new System.Drawing.Point(293, 36);
-            this.LblMineCounter.Name = "LblMineCounter";
-            this.LblMineCounter.Size = new System.Drawing.Size(136, 29);
-            this.LblMineCounter.TabIndex = 2;
-            this.LblMineCounter.Text = "Bombs: 16";
-            this.LblMineCounter.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // LblGameStatus
-            // 
-            this.LblGameStatus.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.LblGameStatus.AutoSize = true;
-            this.LblGameStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LblGameStatus.ForeColor = System.Drawing.Color.ForestGreen;
-            this.LblGameStatus.Location = new System.Drawing.Point(293, 77);
-            this.LblGameStatus.Name = "LblGameStatus";
-            this.LblGameStatus.Size = new System.Drawing.Size(117, 29);
-            this.LblGameStatus.TabIndex = 3;
-            this.LblGameStatus.Text = "You Win!";
-            this.LblGameStatus.Visible = false;
             // 
             // Game
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Gray;
-            this.ClientSize = new System.Drawing.Size(441, 517);
-            this.Controls.Add(this.LblGameStatus);
-            this.Controls.Add(this.LblMineCounter);
-            this.Controls.Add(this.NudBombCounter);
-            this.Controls.Add(this.GameStatus);
+            this.ClientSize = new System.Drawing.Size(863, 517);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Game";
             this.Text = "Minesweeper";
             this.SizeChanged += new System.EventHandler(this.Game_SizeChanged);
-            ((System.ComponentModel.ISupportInitialize)(this.GameStatus)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.NudBombCounter)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox GameStatus;
-        private System.Windows.Forms.NumericUpDown NudBombCounter;
-        private System.Windows.Forms.Label LblMineCounter;
-        private System.Windows.Forms.Label LblGameStatus;
     }
 }
 
--- Game.cs
+++ Game.cs
@@ -4,444 +4,127 @@ using System.Windows.Forms;
 
 namespace MineSweeper
 {
-    public partial class Game : Form
-    {
-        Random rand = new Random(); // module is used to distribute mines.
-        int w = 10, h = 10, s = 34, m = 0; // Dimensions of button: w=width, h=height, s=size, m=margin
-        Button b; // The properites of the box that was pressed.
-        Point p; // The coordinate of the box that was pressed.
-        readonly Color hidden = Color.LightGray;
-        readonly Color[] palette =
-            {
-                Color.DarkGray,
-                Color.Blue,
-                Color.Green,
-                Color.Red,
-                Color.Purple,
-                Color.Maroon,
-                Color.Turquoise,
-                Color.Black,
-                Color.DimGray,
-            };
+	public partial class Game : Form
+	{
+		struct Node
+		{
+			public Button land { get; set; }
+			public int gCost { get; set; }
+			//public int hCost { get; set; }
+			public Button parent { get; set; }
+		}
 
-        Cell[,] land;
+		Random rand = new Random();
+		int w = 13, h = 5, s = 34, m = 0;
+		int wMult = 3;
+		Button b;
+		readonly Color hidden = Color.LightGray;
+		readonly Color open = Color.GreenYellow;
+		readonly Color closed = Color.Red;
 
-        bool gameOver, first;
-        int flags, mines;
+		int gCost = 0;
+		Point end;
+		Node[,] nodes;
 
-        //Initilize
-        public Game()
+		//Initilize
+		public Game()
+		{
+			InitializeComponent();
+			end = new Point(w - 1, h - 1);
+			nodes = new Node[w, h];
+
+			for (int y = 0; y < h; y++)
+			{
+				for (int x = 0; x < w; x++)
+				{
+					nodes[x, y].land = new Button
+					{
+						Anchor = AnchorStyles.Top | AnchorStyles.Left,
+						Font = new Font("Microsoft sans serif", 12, FontStyle.Regular),
+						BackColor = hidden,
+						Location = new Point(x * (s * wMult), s * y),
+						Size = new Size(s * wMult, s),
+						Tag = new Point(x, y),
+					};
+					nodes[x, y].gCost = 0;
+					Controls.Add(nodes[x, y].land);
+                    nodes[x, y].land.MouseDown += Land_MouseDown; ;
+				}
+			}
+			nodes[0, 0].land.BackColor = open;
+			Game_SizeChanged(new object(), new EventArgs());
+		}
+
+		private void Land_MouseDown(object sender, MouseEventArgs e)
+		{
+			b = (Button)sender;
+
+			if (MouseButtons == MouseButtons.Left)
+			{
+				if (b.BackColor == open)
+				{
+					b.BackColor = closed;
+					LeftClick(b);
+				}
+			}
+
+			if (MouseButtons == MouseButtons.Right)
+			{
+				if (b.BackColor == hidden)
+				{
+					b.BackColor = closed;
+				}
+			}
+		}
+
+		private void Game_SizeChanged(object sender, EventArgs e)
+		{
+			for (int y = 0; y < h; y++)
+			{
+				for (int x = 0; x < w; x++)
+				{
+					nodes[x, y].land.Location = new Point(x * s * wMult, s * y);
+				}
+			}
+		}
+
+		void LeftClick(Button b)
+		{
+			Point p = (Point)b.Tag;
+
+			for (int i = 0; i < 9; i++)
+			{
+				Point n = new Point(p.X - 1 + i % 3, p.Y - 1 + i / 3);
+				if (i == 4 || n.X >= 0 && n.X < w && n.Y >= 0 && n.Y < h)
+					if (nodes[n.X, n.Y].land.BackColor == closed)
+						continue;
+					else
+					{
+						nodes[n.X, n.Y].land.BackColor = open;
+						int step = (p.X - n.X + (p.Y - n.Y)) % 2 == 0 ? 14 : 10;
+						if (nodes[p.X, p.Y].gCost + step < nodes[n.X, n.Y].gCost || nodes[n.X, n.Y].gCost == 0)
+						{
+							nodes[n.X, n.Y].gCost = nodes[p.X, p.Y].gCost + step;
+							nodes[n.X, n.Y].parent = b;
+						}
+
+						//prints the gCost
+						nodes[n.X, n.Y].land.Text = $"{nodes[n.X, n.Y].gCost + (int)(Math.Round(Math.Sqrt(Math.Pow(n.X - end.X, 2) + Math.Pow(n.Y - end.Y, 2)), 2) * 10)}:{((Point)nodes[n.X, n.Y].parent.Tag).X},{((Point)nodes[n.X, n.Y].parent.Tag).Y}";
+					}
+			}
+		}
+		void computegCost(Point n, Button parent, bool diag)
         {
-            InitializeComponent();
-            NudBombCounter.Maximum = w * h - 13 >= 0 ? w * h - 13 : 0;
-            mines = (int)NudBombCounter.Value; // Number of mines on the board.
-            LblMineCounter.Text = $"bombs: {mines - flags}";
-            flags = 0; // Number of flags used.
-            first = true; // Boolean to let the mines be placed once, and asuring that you won't sweep a mine on first sweep.
-            gameOver = false; // Locking the game state on game over.
-            land = new Cell[w, h];
-
-            for (int y = 0; y < h; y++)
+			int step = diag ? 14 : 10;
+			int g = nodes[n.X, n.Y].gCost;
+			
+			if (g > gCost + step)
             {
-                for (int x = 0; x < w; x++)
-                {
-                    if (!IsCorner(x, y))
-                    {
-                        land[x, y] = new Cell();
-                        land[x, y].Btn = new Button
-                        {
-                            Anchor = AnchorStyles.Top | AnchorStyles.Left,
-                            Font = new Font("Microsoft sans serif", s - 19, FontStyle.Regular), // Fontsize: 16, Size: 35
-                            BackColor = hidden,
-                            Size = new Size(s, s),
-                            Tag = new Point(x, y),
-                        };
-
-
-                        Controls.Add(land[x, y].Btn);
-                        land[x, y].Btn.MouseDown += Game_MouseDown;
-                    }
-                }
+				nodes[n.X, n.Y].gCost = gCost + step;
+				nodes[n.X, n.Y].parent = parent;
+				gCost += step;
             }
-
-            Game_SizeChanged(new object(), new EventArgs());
-        }
-
-        private void Game_MouseDown(object sender, MouseEventArgs e)
-        {
-            if (!gameOver)
-            {
-                b = (Button)sender;
-                p = (Point)b.Tag;
-
-                if (MouseButtons == MouseButtons.Left)
-                {
-                    if (b.BackColor == hidden)
-                        LeftClick();
-                    else if (b.BackColor == Revealed(0))
-                        SmartClick(land[((Point)b.Tag).X, ((Point)b.Tag).Y]);
-                }
-
-                if (MouseButtons == MouseButtons.Middle)
-                {
-                    if (b.BackColor == Revealed(0))
-                        SmartClick(land[((Point)b.Tag).X, ((Point)b.Tag).Y]);
-                }
-
-                if (MouseButtons == MouseButtons.Right)
-                {
-                    if (b.BackColor != Revealed(0) && !first)
-                    {
-                        if (b.Text != "F")
-                        {
-                            b.ForeColor = Color.Red;
-                            flags++;
-                            b.Text = "F";
-                        }
-                        else
-                        {
-                            b.ForeColor = b.BackColor;
-                            flags--;
-                            b.Text = land[p.X, p.Y].Count.ToString();
-                        }
-                        //smartFlag();
-                    }
-                    LblMineCounter.Text = $"bombs: {mines - flags}";
-                }
-            }
-        }
-
-        //Start new puzzle
-        private void GameStatus_Click(object sender, EventArgs e)
-        {
-            rand = new Random();
-            first = true;
-            flags = 0;
-            mines = (int)NudBombCounter.Value;
-            LblMineCounter.Text = $"bombs: {mines - flags}";
-            LblGameStatus.Visible = false;
-            GameStatus.Image = Properties.Resources.playing;
-
-            for (int y = 0; y < h; y++)
-            {
-                for (int x = 0; x < w; x++)
-                {
-                    if (!IsCorner(x, y) && land[x, y].Btn.ForeColor != Color.White)
-                    {
-                        land[x, y].Btn.BackColor =
-                        land[x, y].Btn.ForeColor = hidden;
-                        land[x, y].IsMine = false;
-                    }
-                }
-            }
-            first = true;
-            gameOver = false;
-        }
-
-        //For bigger puzzles
-        private void Game_SizeChanged(object sender, EventArgs e)
-        {
-            for (int y = 0; y < h; y++)
-            {
-                for (int x = 0; x < w; x++)
-                {
-                    if (!((x == 0 || x == w - 1) && y == 0 ||
-                        (x == 0 || x == w - 1) && y == h - 1))
-                        land[x, y].Btn.Location = new Point((Width - (w + 1) * s) / 2 + x * (s + m), 128 + y * (s + m));
-                }
-            }
-
-            NudBombCounter.Location = new Point(Width / 2 - NudBombCounter.Size.Width - GameStatus.Size.Width - 6, 87);
-            GameStatus.Location = new Point(NudBombCounter.Location.X + NudBombCounter.Size.Width + 6, 23);
-            LblMineCounter.Location = new Point(GameStatus.Location.X + GameStatus.Size.Width + 6, 30);
-            LblGameStatus.Location = new Point(GameStatus.Location.X + GameStatus.Size.Width + 6, 77);
-        }
-
-        //places mines
-        void First()
-        {
-            Point p = (Point)b.Tag;
-            int ned, sid, placed = 0;
-            bool avalible = false;
-
-            while (placed < mines)
-            {
-                ned = rand.Next(h);
-                sid = rand.Next(w);
-
-                if (IsAvalible(sid, ned) &&
-                    !land[sid, ned].IsMine)
-                {
-                    avalible = true;
-                    for (int i = 0; i < 9; i++)
-                    {
-                        if (p.X + i % 3 - 1 == sid &&
-                            p.Y + i / 3 - 1 == ned)
-                        {
-                            avalible = false;
-                        }
-                    }
-                    if (avalible)
-                    {
-                        land[sid, ned].IsMine = true;
-                        land[sid, ned].Btn.Text = "X";
-                        placed++;
-                    }
-                }
-            }
-
-            for (int y = 0; y < h; y++)
-            {
-                for (int x = 0; x < w; x++)
-                {
-                    if (IsAvalible(x, y))
-                    {
-                        land[x, y].Count = 0;
-
-                        for (int i = 0; i < 9; i++)
-                        {
-                            if (land[x, y].IsMine) break;
-                            if (i != 4 &&
-                                IsWithinGrid(x, y, i) &&
-                                !IsCorner(x, y, i) &&
-                                    land[x + i % 3 - 1, y + i / 3 - 1].IsMine) land[x, y].Count++;
-                        }
-                        land[x, y].Btn.Text = land[x, y].Count.ToString();
-                        if (x != p.X || y != p.Y) land[x, y].Btn.ForeColor = b.BackColor;
-                        else b.ForeColor = Revealed(land[p.X, p.Y].Count);
-                    }
-                }
-            }
-            first = false;
-        }
-
-        void LeftClick()
-        {
-            Point p = (Point)b.Tag;
-
-            if (first)
-                First();
-
-            Sweep(p.X, p.Y); // check for Lose(x, y)
-
-            //check for win
-            Win();
-        }
-
-        //to skip a few clicks
-        void SmartClick(Cell l)
-        {
-            Point p = (Point)l.Btn.Tag;
-            int flags = 0;
-            for (int i = 0; i < 9; i++)
-            {
-                if (IsAvalible(p.X, p.Y, i) &&
-                    land[p.X + i % 3 - 1, p.Y + i / 3 - 1].Btn.Text == "F") flags++;
-            }
-            if (flags == land[p.X, p.Y].Count)
-            {
-                for (int i = 0; i < 9; i++)
-                    if (i != 4 &&
-                        IsAvalible(p.X, p.Y, i) &&
-                        land[p.X + i % 3 - 1, p.Y + i / 3 - 1].Btn.Text != "F")
-                        {
-                            if (!land[p.X + i % 3 - 1, p.Y + i / 3 - 1].IsMine)
-                                Sweep(p.X + i % 3 - 1, p.Y + i / 3 - 1);
-                            else
-                            {
-                                Lose(p.X + i % 3 - 1, p.Y + i / 3 - 1);
-                                return;
-                            }
-                        }
-            }
-
-            //check for win
-            Win();
-        }
-
-        //Recursivly sweeps all adjacent land.
-        void Sweep(int x, int y)
-        {
-            if (land[x, y].IsMine && land[x, y].Btn.Text != "F") Lose(x, y);
-
-            if (land[x, y].Btn.BackColor == Revealed(0) ||
-                land[x, y].Btn.Text == "F" ||
-                gameOver) { return; }
-            
-            if (land[x, y].Btn.BackColor == hidden)
-            {
-                land[x, y].Btn.BackColor = Revealed(0);
-                land[x, y].Btn.ForeColor = Revealed(int.Parse(land[x, y].Btn.Text));
-
-                if (land[x, y].Btn.Text == "0")
-                    for (int i = 0; i < 9; i++)
-                        if (IsAvalible(x, y, i))
-                        {
-                            if (!land[x + i % 3 - 1, y + i / 3 - 1].IsMine)
-                                Sweep(x + i % 3 - 1, y + i / 3 - 1);
-                            else
-                            {
-                                Lose(x + i % 3 - 1, y + i / 3 - 1);
-                                return;
-                            }
-                        }
-            }
-            return;
-        }
-
-        /// <summary>
-        /// IsWithinGrid && !IsCorner
-        /// </summary>
-        /// <param name="x"></param>
-        /// <param name="y"></param>
-        /// <returns></returns>
-        bool IsAvalible (int x, int y)
-        {
-            // is within grid
-            bool a = IsWithinGrid(x, y);
-
-            // is not corner
-            bool c = !IsCorner(x, y);
-
-            return a && c;
-        }
-
-        bool IsAvalible (int x, int y, int i)
-        {
-            // is within grid
-            bool a = IsWithinGrid(x, y, i);
-
-            // is not corner
-            bool c = !IsCorner(x, y, i);
-
-            return a && c;
-        }
-
-        bool IsWithinGrid(int x, int y) => x < w && y < h && x >= 0 && y >= 0;
-        bool IsWithinGrid(int x, int y, int i) => x + i % 3 - 1 < w && y + i / 3 - 1 < h && x + i % 3 - 1 >= 0 && y + i / 3 - 1 >= 0;
-
-        bool IsCorner(int x, int y) => (x == 0 || x == w - 1) && y == 0 || (x == 0 || x == w - 1) && y == h - 1;
-        bool IsCorner(int x, int y, int i)
-        {
-            x = x + i % 3 - 1;
-            y = y + i / 3 - 1;
-            return (x == 0 || x == w - 1) && y == 0 || (x == 0 || x == w - 1) && y == h - 1;
-        }
-
-        void SmartFlag()
-        {
-            for (int i = 0; i < 9; i++)
-            {
-                int px = p.X + i % 3 - 1;
-                int py = p.Y + i / 3 - 1;
-
-                if (px < w &&
-                    py < h &&
-                    px >= 0 &&
-                    py >= 0 &&
-                    i != 4 &&
-                    land[px, py].Btn.Text != "F" &&
-                    int.Parse(land[px, py].Btn.Text) >= 0)
-                {
-                    land[px, py].Count += b.Text == "F" ? -1 : 1;
-                    land[px, py].Btn.Text = land[px, py].Count.ToString();
-                    land[px, py].Btn.ForeColor = land[px, py].Btn.ForeColor == hidden ? hidden : Revealed(land[px, py].Count);
-                }
-            }
-        }
-
-        //Check for win
-        void Win()
-        {
-            int revealed = 0;
-            for (int y = 0; y < h; y++)
-                for (int x = 0; x < w; x++)
-                    if (!IsCorner(x, y) && land[x, y].Btn.BackColor == Revealed(0))
-                        revealed++;
-
-            if (revealed == w * h - mines - 4)
-            {
-                for (int y = 0; y < h; y++)
-                {
-                    for (int x = 0; x < w; x++)
-                    {
-                        if (!IsCorner(x, y))
-                        {
-                            if (land[x, y].IsMine &&
-                                land[x, y].Btn.Text != "F")
-                            {
-                                land[x, y].Btn.ForeColor = Color.Red;
-                                flags++;
-                                land[x, y].Btn.Text = "F";
-                            }
-                        }
-                    }
-                }
-                LblMineCounter.Text = $"bombs: {mines - flags}";
-                GameState(true);
-            }
-        }
-
-        void Lose(int x, int y)
-        {
-            land[x, y].Btn.BackColor = Color.Red;
-
-            for (int ned = 0; ned < h; ned++)
-            {
-                for (int sid = 0; sid < w; sid++)
-                {
-                    if (!IsCorner(sid, ned))
-                    {
-                        if (land[sid, ned].IsMine && land[sid, ned].Btn.Text != "F")
-                        {
-                            land[sid, ned].Btn.ForeColor = Color.Black;
-                            land[sid, ned].Btn.Text = "X";
-                        }
-                        else if (!land[sid, ned].IsMine && land[sid, ned].Btn.Text == "F")
-                        {
-                            land[sid, ned].Btn.ForeColor = Color.Blue;
-                            land[sid, ned].Btn.Text = ">";
-                        }
-                    }
-                }
-            }
-            GameState(false);
-        }
-
-        void GameState(bool win)
-        {
-            LblGameStatus.Visible = true;
-            gameOver = true;
-            LblMineCounter.Text = "< - Press me!";
-
-            if (win)
-            {
-                LblGameStatus.ForeColor = Color.ForestGreen;
-                LblGameStatus.Text = "You Win!";
-                GameStatus.Image = Properties.Resources.win;
-            }
-            else
-            {
-                LblGameStatus.ForeColor = Color.DarkRed;
-                LblGameStatus.Text = "Game Over";
-                GameStatus.Image = Properties.Resources.lost;
-            }
-        }
-
-        Color Revealed(int Component)
-        {
-            int c = Component;
-            
-            for (int g = 0; g < palette.Length; g++)
-                if (c == g) return palette[g];
-            return new Color();
-        }
-    }
-
-    class Cell
-    {
-        public Button Btn { get; set; }
-        public int Count { get; set; }
-        public bool IsMine { get; set; }
-    }
+			//Text = $"{parent.Tag}, {gCost}";
+		}
+	}
 }
